d9b7f820f43e71d61d2086827337bf24172ed880
Paulina Kisielewska <paulina.kisielewska6@gmail.com>
2024-02-28 18:02:06 +0100

    Booking start

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,20 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @pet = Pet.find(params[:pet_id])
+    @booking.pet = @pet
+    if @booking.save
+      redirect_to pet_path(@pet)
+    else
+      # @bookmark = Bookmark.new
+      render "/", status: :unprocessable_entity
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
